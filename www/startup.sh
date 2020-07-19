f37aa87bb64ab2b472e411e81dc79ad3eb40761c
amenoyoya <mtyswe3@gmail.com>
2020-07-19 21:10:21 +0900

    🐛 fix: docker構成修正

--- www/startup.sh
+++ www/startup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-# -- www-data@docker://web/
+# -- sudo www-data@docker://web/
 
 # 環境変数 UID が与えられていれば www-data ユーザIDを $UID に合わせる
 if [ "$UID" != "" ]; then
     # uid = $UID のダミーユーザ作成
     if [ "$(getent passwd $UID)" = "" ]; then
-        sudo useradd -u $UID -g $UID myuser
+        useradd -u $UID -g $UID myuser
     fi
     # www-data ユーザIDを変更
-    sudo usermod -u $UID www-data
-    sudo groupmod -g $UID www-data
+    usermod -u $UID www-data
+    groupmod -g $UID www-data
     # www-data のホームディレクトリのパーミッション修正
-    sudo chown -R www-data:www-data /var/www/
+    chown -R www-data:www-data /var/www/
 fi
 
 # ~/.msmtprc のパーミッション修正
-sudo chown www-data:www-data /var/www/.msmtprc
-sudo chmod 600 /var/www/.msmtprc
+chown www-data:www-data /var/www/.msmtprc
+chmod 600 /var/www/.msmtprc
 
 # プロジェクトディレクトリ作成
 if [ ! -d './app' ]; then
@@ -24,5 +24,5 @@ if [ ! -d './app' ]; then
     echo '<?php phpinfo() ?>' > ./app/public/index.php
 fi
 
-# 環境変数を引き継いで Apache を起動させる
-sudo -E apachectl -D FOREGROUND
+# Apache をフォアグランド起動
+apachectl -D FOREGROUND
